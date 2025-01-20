6f38a80da1b34368a11d0206ea031b24a695a158
Hao Nghiem <qtvhao@gmail.com>
2025-01-20 11:30:42 +0700

    Enhance DownloadCommandHandler to support file retrieval from peer and relay services

--- services/gateway/src/Application/CommandHandlers/download_command_handler.py
+++ services/gateway/src/Application/CommandHandlers/download_command_handler.py
@@ -1,6 +1,8 @@
 from Application.Commands.download_command import DownloadCommand
 from Application.Commands.broadcast_command import BroadcastCommand
 from Infrastructure.Services.s3_get_file_service import S3GetFileService
+from Infrastructure.Services.peer_get_file_service import PeerGetFileService
+from Infrastructure.Services.relay_get_file_service import RelayGetFileService
 from Application.CommandHandlers.broadcast_command_handler import BroadcastCommandHandler
 from fastapi import Depends
 
@@ -9,10 +11,14 @@ class DownloadCommandHandler:
     def __init__(
         self,
         broadcast_command_handler: BroadcastCommandHandler = Depends(),
+        peer_get_file_service: PeerGetFileService = Depends(),
+        relay_get_file_service: RelayGetFileService = Depends(),
         s3_get_file_service: S3GetFileService = Depends()
     ):
         self.s3_get_file_service = s3_get_file_service
         self.broadcast_command_handler = broadcast_command_handler
+        self.peer_get_file_service = peer_get_file_service
+        self.relay_get_file_service = relay_get_file_service
 
     def handle(self, command: DownloadCommand) -> bytes:
         file_contents = self.s3_get_file_service.get_file_contents(command.cid)
@@ -20,7 +26,14 @@ class DownloadCommandHandler:
             broadcast_command = BroadcastCommand(
                 cid=command.cid
             )
-            node = self.broadcast_command_handler.handle(broadcast_command)
-            print(f"Node {node['uuid']} has data for CID {command.cid}")
+            provider_peer = self.broadcast_command_handler.handle(broadcast_command)
+            print(f"Node {provider_peer['uuid']} has data for CID {command.cid}")
+            file_contents = self.peer_get_file_service.get_file_contents(provider_peer['uuid'], command.cid)
+
+        if file_contents is None:
+            file_contents = self.relay_get_file_service.get_file_contents(provider_peer['uuid'], command.cid)
+
+        if file_contents is None:
+            raise Exception(f"Could not download file with CID {command.cid}")
 
         return file_contents
---
+++ services/gateway/src/Infrastructure/Services/peer_get_file_service.py
@@ -0,0 +1,3 @@
+class PeerGetFileService:
+    def get_file_contents(self, peer_uuid: str, file_key: str) -> bytes:
+        return None
