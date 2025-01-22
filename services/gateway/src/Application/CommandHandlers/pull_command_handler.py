c78b8f71df4dd1f91976e5597d2bd4da751c09a3
Hao Nghiem <qtvhao@gmail.com>
2025-01-23 00:31:51 +0700

    Add Gzip compression and Base64 encoding to file download in PullCommandHandler

--- services/gateway/src/Application/CommandHandlers/pull_command_handler.py
+++ services/gateway/src/Application/CommandHandlers/pull_command_handler.py
@@ -2,6 +2,9 @@ from Application.Commands.download_command import DownloadCommand
 from Infrastructure.Services.s3_get_file_service import S3GetFileService
 from Infrastructure.Services.local_cache_get_file_service import LocalCacheGetFileService
 from fastapi import Depends
+import base64
+import gzip
+from io import BytesIO
 
 class PullCommandHandler:
     def __init__(
@@ -21,4 +24,13 @@ class PullCommandHandler:
         if file_contents is None:
             raise Exception(f"Could not download file with CID {command.cid}")
 
-        return file_contents
+        # Compress the file with Gzip
+        gzip_buffer = BytesIO()
+        with gzip.GzipFile(fileobj=gzip_buffer, mode="wb") as gzip_file:
+            gzip_file.write(file_contents)
+
+        # Encode the Gzip-compressed content in Base64
+        compressed_data = gzip_buffer.getvalue()
+        base64_encoded_data = base64.b64encode(compressed_data)
+
+        return base64_encoded_data
