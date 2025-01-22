7843eae748a253379a95e22a8b01fc2a298fe392
Hao Nghiem <qtvhao@gmail.com>
2025-01-22 10:06:47 +0700

    Base files into script

--- make-segments.sh
+++ make-segments.sh
@@ -1,1 +1,3 @@
-ffmpeg -i LwekZs3Sp8g.webm -c:v libx264 -crf 23 -vf scale=320:240 -c:a aac -b:a 64k -strict experimental -hls_time 15 -hls_playlist_type vod -hls_segment_filename "segments/segment_%03d.ts" ./segments/output.m3u8
+rm segments/*
+ffmpeg -i LwekZs3Sp8g.webm -c:v libx264 -crf 23 -vf scale=176:144 -c:a aac -b:a 64k -strict experimental -hls_time 5 -hls_playlist_type vod -hls_segment_filename "segments/segment_%03d.ts" ./segments/output.m3u8
+ls -lh ./segments/
