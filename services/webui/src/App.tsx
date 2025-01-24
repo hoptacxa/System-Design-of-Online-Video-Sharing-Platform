f62d3bfd27200013e8bca422ab3bbbbe24e0586d
Hao Nghiem <qtvhao@gmail.com>
2025-01-24 10:54:53 +0700

    Refactor App component to simplify layout and remove unused code

--- services/webui/src/App.tsx
+++ services/webui/src/App.tsx
@@ -1,7 +1,5 @@
 import { io, Socket } from 'socket.io-client';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
@@ -70,45 +68,11 @@ function App() {
     }
   });
 
-  const [count, setCount] = useState(0)
-  // Kích thước chuỗi cần tạo (5MB)
-  const sizeInBytes = 8 * 1024 * 1024; // 5MB
-  const sizeInChars = sizeInBytes / 2; // Mỗi ký tự chiếm 2 byte
-
-  // Tạo chuỗi nhỏ để lặp lại
-  const smallString = "a"; // 1 ký tự
-  const repeatCount = Math.ceil(sizeInChars / smallString.length);
-
-  // Tạo chuỗi 5MB
-  const largeString = smallString.repeat(repeatCount).slice(0, sizeInChars);
-
-  localStorage.setItem("keyname", largeString)
-  // 
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        <video controls>
-          <source type="application/x-mpegURL" src="https://http-0-0-0-0-3001.schnworks.com/command/get_by_name/LwekZs3Sp8g/output.m3u8" />
-        </video>
-      </p>
+      <video controls>
+        <source type="application/x-mpegURL" src="https://http-0-0-0-0-3001.schnworks.com/command/get_by_name/LwekZs3Sp8g/output.m3u8" />
+      </video>
     </>
   )
 }
